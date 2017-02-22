563b969031bbf6dd7bad9750e573de7fa1a0ed66
ELEMENT\user <zestymoment@gmail.com>
2017-02-22 20:16:54 +0300

    change in: finally added class XmlValues

---
+++ src/main/java/requests/XmlValues.java
@@ -0,0 +1,85 @@
+package requests;
+
+public class XmlValues {
+    private String itemTitleValue;
+    private String itemNoteValue;
+    private String itemQuantityValue;
+    private String itemPriceValue;
+    private String orderPersonValue;
+    private String shipToNameValue;
+    private String shipToAddressValue;
+    private String shipToCityValue;
+    private String shipToCountryValue;
+
+    public String getItemTitleValue() {
+        return itemTitleValue;
+    }
+
+    public String getItemNoteValue() {
+        return itemNoteValue;
+    }
+
+    public String getItemQuantityValue() {
+        return itemQuantityValue;
+    }
+
+    public String getItemPriceValue() {
+        return itemPriceValue;
+    }
+
+    public String getOrderPersonValue() {
+        return orderPersonValue;
+    }
+
+    public String getShipToNameValue() {
+        return shipToNameValue;
+    }
+
+    public String getShipToAddressValue() {
+        return shipToAddressValue;
+    }
+
+    public String getShipToCityValue() {
+        return shipToCityValue;
+    }
+
+    public String getShipToCountryValue() {
+        return shipToCountryValue;
+    }
+
+    public void setItemTitleValue(String textContent) {
+        this.itemTitleValue = textContent;
+    }
+
+    public void setItemNoteValue(String textContent) {
+        this.itemNoteValue = textContent;
+    }
+
+    public void setItemQuantityValue(String textContent) {
+        this.itemQuantityValue = textContent;
+    }
+
+    public void setItemPriceValue(String textContent) {
+        this.itemPriceValue = textContent;
+    }
+
+    public void setOrderPersonValue(String textContent) {
+        this.orderPersonValue = textContent;
+    }
+
+    public void setShipToNameValue(String textContent) {
+        this.shipToNameValue = textContent;
+    }
+
+    public void setShipToAddressValue(String textContent) {
+        this.shipToAddressValue = textContent;
+    }
+
+    public void setShipToCityValue(String textContent) {
+        this.shipToCityValue = textContent;
+    }
+
+    public void setShipToCountryValue(String textContent) {
+        this.shipToCountryValue = textContent;
+    }
+}
